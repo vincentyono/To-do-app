cdbf526fcd173b5e942daa179028e226e5d35808
vincentyono <vincentyono@protonmail.com>
2023-03-10 18:05:10 +0700

    Implement Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.footer`
+  text-align: center;
+`;
+
+export default function Footer() {
+  return (
+    <Container>
+      <address>Vincent Yono - 2502009583</address>
+    </Container>
+  );
+}
