eeb1eb625ca58ec260a39244caa71616f5ce567a
vincentyono <vincentyono@protonmail.com>
2023-03-10 18:03:36 +0700

    Import fonts using vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginFonts } from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'Poppins',
+            styles: '@400,600,900',
+          },
+        ],
+      },
+    }),
+  ],
+});
