bb66504fec7d62cb25384125c4bef0ae1a983b00
vincentyono <vincentyono@protonmail.com>
2023-03-16 18:23:50 +0700

    Implement react router

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,35 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { ChakraProvider } from '@chakra-ui/react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom';
 import './global.css';
 import 'virtual:fonts.css';
+import Home from './pages/Home';
+import Header from './components/Header';
+
+const Root = () => {
+  return <Outlet />;
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <ChakraProvider>
-        <App />
+        <RouterProvider router={router} />
       </ChakraProvider>
     </Provider>
   </React.StrictMode>
