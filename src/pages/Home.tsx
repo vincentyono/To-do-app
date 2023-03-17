536487069c4d2300e8c8f6a4ad65209fb91ec5c7
vincentyono <vincentyono@protonmail.com>
2023-03-17 09:29:47 +0700

    Edit Home page styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,9 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import GetStarted from '/get-started.svg';
-import { Button } from '@chakra-ui/react';
+import { Button, Container, Stack } from '@chakra-ui/react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  max-width: 20%;
-  margin-block: 5rem;
-  margin-inline: auto;
-
-  @media (max-width: 1320px) {
-    max-width: 30%;
-  }
-
-  @media (max-width: 720px) {
-    max-width: 50%;
-  }
-
-  @media (max-width: 600px) {
-    max-width: 60%;
-  }
-
-  @media (max-width: 520px) {
-    max-width: 65%;
-  }
-
-  @media (max-width: 420px) {
-    max-width: 80%;
-  }
-
-  @media (max-width: 360px) {
-    max-width: 85%;
-  }
-`;
-
-const Image = styled.img``;
 
 const H2 = styled.h2`
   text-align: center;
@@ -52,20 +18,22 @@ export default function Home() {
   return (
     <>
       <Header>To do App</Header>
-      <Main>
-        <Image src={GetStarted} alt='cat' />
+      <Container as='main' maxW='md' marginBlock='3rem 1.5rem'>
+        <img src={GetStarted} alt='cat' />
         <H2>A simple To do app</H2>
-        <Button
-          as={Link}
-          to={'/login'}
-          bg='#6C63FF'
-          color='white'
-          marginBlock='2rem'
-          _hover={{ backgroundColor: '#5c56c0' }}
-        >
-          Get started
-        </Button>
-      </Main>
+        <Stack paddingInline='3.5rem'>
+          <Button
+            as={Link}
+            to={'/signin'}
+            bg='#6C63FF'
+            color='white'
+            marginBlock='2rem'
+            _hover={{ backgroundColor: '#5c56c0' }}
+          >
+            Get started
+          </Button>
+        </Stack>
+      </Container>
       <Footer />
     </>
   );
