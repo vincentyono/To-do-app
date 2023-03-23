6dbed4d949ff3949465f7bf00b9d673dc29dc356
vincentyono <vincentyono@protonmail.com>
2023-03-23 21:15:16 +0700

    Change logout to signout

--- src/pages/Signout.tsx
+++ src/pages/Signout.tsx
@@ -1,14 +1,27 @@
-import { Alert, AlertIcon, AlertTitle, Container } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Container,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function Logout() {
+export default function Signout() {
+  const [seconds, setSeconds] = useState<number>(3);
   const navigate = useNavigate();
   useEffect(() => {
-    setTimeout(() => {
+    if (seconds === 0) {
       navigate('/');
-    }, 3000);
-  }, []);
+      return;
+    }
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [seconds]);
 
   return (
     <Container marginBlock='auto'>
@@ -26,6 +39,9 @@ export default function Logout() {
         <AlertTitle mt={4} mb={1} fontSize='lg'>
           Logout Successful!
         </AlertTitle>
+        <AlertDescription>
+          Redirecting to Home page in {seconds} seconds...
+        </AlertDescription>
       </Alert>
     </Container>
   );
